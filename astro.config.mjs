33c0b9c2718faade086cc453d8ad2cbf2f4c9b29
cassio7 <acassieri@gmail.com>
2025-10-01 22:08:53 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alessiocassieri.netlify.app/',
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
